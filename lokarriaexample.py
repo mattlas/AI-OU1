f65a20938c0b62ee6ce8dfb1fe9b754f5786d8c8
Mattias Johansson <mattiasjohansson001@gmail.com>
2015-09-10 15:00:22 +0200

    comment off

--- lokarriaexample.py
+++ lokarriaexample.py
@@ -117,7 +117,7 @@ if __name__ == '__main__':
     data = json.loads(json_data)
     """pprint(data)"""
 
-    print 'Sending commands to MRDS server', MRDS_URL
+    """print 'Sending commands to MRDS server', MRDS_URL
     try:
         print 'Telling the robot to go streight ahead.'
         response = postSpeed(0,0.1) 
@@ -146,4 +146,4 @@ if __name__ == '__main__':
             print 'Current heading vector: X:{X:.3}, Y:{Y:.3}'.format(**getBearing())
             time.sleep(1)
     except UnexpectedResponse, ex:
-        print 'Unexpected response from server when reading position:', ex
+        print 'Unexpected response from server when reading position:', ex"""
